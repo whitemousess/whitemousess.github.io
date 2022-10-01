af9f90ffaca343be6c6f6a85fb6bae27bfbf3eb0
whitemousess <94060884+whitemousess@users.noreply.github.com>
2022-10-01 18:17:59 +0700

    1/10/2022

--- assets/css/scrip.js
+++ assets/css/scrip.js
@@ -1,77 +1,59 @@
 var mobileMenu = document.getElementById('mobile-menu-btn');
-        var headerHeight = header.clientHeight;
+var headerHeight = header.clientHeight;
 
-        //close/open menu icon mobile
-    mobileMenu.onclick = function(){
-        var isClosed = header.clientHeight === headerHeight;
-        if(isClosed){
-            header.style.height = 'auto';
-        }
-        else{
-            header.style.height = null;
-        }
+//close/open menu icon mobile
+mobileMenu.onclick = function () {
+    var isClosed = header.clientHeight === headerHeight;
+    if (isClosed) {
+        header.style.height = 'auto';
     }
+    else {
+        header.style.height = null;
+    }
+}
 
-    //auto closed when clicking
-    var menuItems = document.querySelectorAll('#nav li a[href*="#"]');
-    for(var i =0; i<menuItems.length; i++){
-        var menuItem = menuItems[i];
-        
-        menuItem.onclick = function(event){
+//auto closed when clicking
+var menuItems = document.querySelectorAll('#nav li a[href*="#"]');
+for (var i = 0; i < menuItems.length; i++) {
+    var menuItem = menuItems[i];
+
+    menuItem.onclick = function (event) {
         var isParentMenu = this.nextElementSibling
-        && this.nextElementSibling.classList.contains('subnav');
-        if(isParentMenu){
+            && this.nextElementSibling.classList.contains('subnav');
+        if (isParentMenu) {
             event.preventDefault();
-        }else{
+        } else {
             header.style.height = null;
         }
-        }
     }
+}
+// ///////////////////////An////////////////////////////
+{var modalAn = document.querySelector('.modalAn');
+var btnOpenAn = document.querySelector(".open-modal-btn-An")
+var iconclose = document.querySelector(".modal-close-An")
 
-    const btnPinhs = document.querySelectorAll('.js-btnPinh')
-    const modalPinh = document.querySelector('.js-Pinh')
-    const modalcontainer = document.querySelector('.js-modal-container')
-    const modalclose = document.querySelector('.js-modal-close')
+function toggleModal(e) {
+    modalAn.classList.toggle('hideAn')
+}
 
-    //hiển thị mua ticket
-    function ShowImgPinh(){
-        modalPinh.classList.add('open')
-    }
-    //ẩn thị mua ticket
-    function hideImgPinh(){
-        modalPinh.classList.remove('open')
-    }
+btnOpenAn.addEventListener('click', toggleModal);
+iconclose.addEventListener('click', toggleModal);
+modalAn.addEventListener("click", (e) => {
+    if (e.target == e.currentTarget) toggleModal();
+  });
+  }
+// //////////////////////////Pinh///////////////////////
+{var modalPinh = document.querySelector('.modalPinh');
+var btnOpenPinh = document.querySelector(".open-modal-btn-Pinh");
+var iconclose = document.querySelector(".modal-close-Pinh");
 
-    //vòng lặp nghe hành vi
-    for (const btnPinh of btnPinhs) {
-        btnPinh.addEventListener('click', ShowImgPinh)
-    }
-     
-    //nghe hành vi click
-    modalclose.addEventListener('click', hideImgPinh)
+function toggleModal(e){
+    modalPinh.classList.toggle('hidePinh')
+}
 
-    modalPinh.addEventListener('click', hideImgPinh)
-
-    modalcontainer.addEventListener('click', function (event){
-        event.stopPropagation()
-    })
-
-    /////////////////JavaScrip an////////////////////////////
-    const btnAns = document.querySelectorAll('.js-btnAn')
-    const modalAn = document.querySelector('.js-An')
-
-    //hiển thị mua ticket
-    function ShowImgAn(){
-        modalAn.classList.add('open')
-    }
-    //ẩn thị mua ticket
-    function hideBuyTickets(){
-        modalAn.classList.remove('open')
-    }
-
-    //vòng lặp nghe hành vi
-    for (const btnAn of btnAns) {
-        btnAn.addEventListener('click', ShowImgAn)
-    }
-
-    modalAn.addEventListener('click', hideBuyTickets)
+iconclose.addEventListener('click', toggleModal);
+btnOpenPinh.addEventListener('click', toggleModal);
+modalPinh.addEventListener("click", (e) => {
+    if (e.target == e.currentTarget) toggleModal();
+  });
+}
